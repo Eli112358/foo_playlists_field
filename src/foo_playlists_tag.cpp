f694091c60f7a9064a4fcbf78aed8ecd2432a583
Eli112358 <Eli112358.SARP@hotmail.com>
2017-02-22 19:09:56 -0500

    Added project files and initial source code

---
+++ src/foo_playlists_tag.cpp
@@ -0,0 +1,4 @@
+#include "stdafx.h"
+
+DECLARE_COMPONENT_VERSION("Playlists tagger","0.0.1","About text here");
+VALIDATE_COMPONENT_FILENAME("foo_playlists_tag.dll");
---
+++ src/stdafx.h
@@ -0,0 +1,2 @@
+#pragma once
+#include "U:\SDK\Foobar2000-music-player\foobar2000\SDK\foobar2000.h"
